01b5d35c715f5cae1eb46ea6520b3a4316c46303
Andrea828 <47058304+Andrea828@users.noreply.github.com>
2019-05-05 14:27:02 +0200

    Versione 1.0

---
+++ arduinoTpsit/arduinoTpsit.ino
@@ -0,0 +1,27 @@
+char input_connection;
+long i = 0;
+int analogInPin = A0;
+int sensorValue = 0;
+
+
+// put your setup code here, to run once:
+void setup() {
+  Serial.begin(9600);
+  pinMode(analogInPin, INPUT);
+
+  pinMode(13, OUTPUT);
+
+}
+
+// put your main code here, to run repeatedly:
+void loop() {
+  sensorValue = analogRead(analogInPin);
+  Serial.println(sensorValue);
+  if(sensorValue > 550) {
+    digitalWrite(13, LOW);
+  }
+  else {
+    digitalWrite(13, HIGH); //accendo luce
+  }
+  delay(1000);
+}
